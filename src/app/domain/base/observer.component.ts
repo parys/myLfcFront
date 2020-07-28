ac29e40120acc396e5c4c45d6cbbab09bd0de016
Andrei Kapitanchyk <andrei.kapitanchyk@h2g2.is>
2020-07-28 22:53:30 +0300

    Changed browserlist to .browserlistrc

--- src/app/domain/base/observer.component.ts
+++ src/app/domain/base/observer.component.ts
@@ -2,6 +2,7 @@ import { OnDestroy, Directive } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 // TODO: Add Angular decorator.
+@Directive()
 export abstract class ObserverComponent implements OnDestroy {
 
     subscriptions: Subscription[] = [];
