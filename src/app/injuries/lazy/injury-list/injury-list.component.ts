3e83d9a76175a3a4bc04e49e4754b75ca10a2274
Andrei Kapitanchyk <andrei.kapitanchyk@h2g2.is>
2021-08-21 14:52:27 +0200

    Code clean ups.

--- src/app/injuries/lazy/injury-list/injury-list.component.ts
+++ src/app/injuries/lazy/injury-list/injury-list.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { ActivatedRoute } from '@angular/router';
 
-import { merge, of, fromEvent, Observable, Subscription } from 'rxjs';
+import { merge, of, fromEvent, Observable } from 'rxjs';
 import { startWith, switchMap, map, catchError, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 import { PagedList } from '@domain/models';
@@ -12,7 +12,6 @@ import { InjuryService } from '@injuries/injury.service';
 import { DEBOUNCE_TIME } from '@constants/app.constants';
 import { PAGE, KEYUP } from '@constants/help.constants';
 import { INJURIES_ROUTE } from '@constants/routes.constants';
-import { ConfirmationMessage } from '@notices/shared';
 import { NotifierService } from '@notices/services';
 import { ObserverComponent } from '@domain/base';
 import { Injury } from '@injuries/models/injury.model';
@@ -23,8 +22,7 @@ import { InjuryFilters } from '@injuries/models/injury-filters.model';
     templateUrl: './injury-list.component.html'
 })
 
-export class InjuryListComponent extends ObserverComponent implements AfterViewInit, OnDestroy {
-    private sub: Subscription;
+export class InjuryListComponent extends ObserverComponent implements AfterViewInit {
     public items: Injury[];
     displayedColumns = ['personName', 'startTime', 'endTime', 'description', 'tool'];
 
@@ -33,16 +31,17 @@ export class InjuryListComponent extends ObserverComponent implements AfterViewI
     @ViewChild('nameInput') nameInput: ElementRef;
 
     constructor(private injuryService: InjuryService,
-        private route: ActivatedRoute,
-        private location: Location,
-        private notifier: NotifierService) {
+                private route: ActivatedRoute,
+                private location: Location,
+                private notifier: NotifierService) {
         super();
     }
 
     public ngAfterViewInit(): void {
-        this.sub = this.route.queryParams.subscribe(qParams => {
+        const sub$ = this.route.queryParams.subscribe(qParams => {
             this.paginator.pageIndex = +qParams[PAGE] - 1 || 0;
         });
+        this.subscriptions.push(sub$);
 
         merge(this.sort.sortChange,
             fromEvent(this.nameInput.nativeElement, KEYUP)
@@ -51,7 +50,7 @@ export class InjuryListComponent extends ObserverComponent implements AfterViewI
             .subscribe(() => this.paginator.pageIndex = 0);
 
 
-        merge(this.sort.sortChange, this.paginator.page, fromEvent(this.nameInput.nativeElement, KEYUP)
+        const sub2$ = merge(this.sort.sortChange, this.paginator.page, fromEvent(this.nameInput.nativeElement, KEYUP)
             .pipe(debounceTime(DEBOUNCE_TIME),
                 distinctUntilChanged()))
             .pipe(
@@ -73,16 +72,12 @@ export class InjuryListComponent extends ObserverComponent implements AfterViewI
                 this.items = data;
                 this.updateUrl();
             });
-    }
 
-    public ngOnDestroy(): void {
-        if (this.sub) { this.sub.unsubscribe(); }
+        this.subscriptions.push(sub2$);
     }
 
     public showDeleteModal(injury: Injury): void {
-        const sub$ = this.notifier.confirm(new ConfirmationMessage({
-            title: 'Удалить ?'
-        }))
+        const sub$ = this.notifier.confirmDeletion()
             .subscribe(result => {
                 if (result) {
                     this.delete(injury.id);
