4d30c44533d933e505edf89ea013de7a74c953c9
Andrei Kapitanchyk <andrei.kapitanchyk@h2g2.is>
2021-08-29 00:03:05 +0200

    FIxed issue with server that 'blob is not defined'

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,10 +1,12 @@
 ﻿import { enableProdMode } from '@angular/core';
 
 import { environment } from './environments/environment';
+import { Blob } from 'blob-polyfill';
 
 if (environment.production) {
   enableProdMode();
 }
+global.Blob = Blob;
 
 export { AppServerModule } from './app/app.module.server';
 export { ngExpressEngine } from '@nguniversal/express-engine';
